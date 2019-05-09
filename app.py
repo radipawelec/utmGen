eec4ca80b4c3c254150bb29b4d5b04568c096182
Rafal <pawelec.rafal@gmail.com>
2019-05-09 08:31:31 +0200

    vmj

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import requests
+import os
 
 app = Flask(__name__)
 
@@ -10,6 +13,10 @@ app = Flask(__name__)
 @app.route('/')
 def my_form():
     return render_template('my-form.html')
+
+@app.route('/vmj')
+def my_form_vmj():
+    return render_template('vmj-form.html')
 
 
 @app.route('/', methods=['POST'])
@@ -23,6 +30,15 @@ def my_form_post():
 
     return render_template('result.html', fb=m.fb, cpc=m.cpc, li=m.li, mail=m.mail)
 
+@app.route('/vmj', methods=['POST'])
+def vmj_form_post():
+
+    link = request.form['link']
+    v = MakeUTMVMJ(link)
+
+
+    return render_template('results_vmj.html', link=v.result_link)
+
 class MakeUTMlink:
     def __init__(self, link, campaign, content):
         if content == "":
@@ -35,6 +51,16 @@ class MakeUTMlink:
             self.cpc = link + "?umt_source=facebook&utm_medium=cpc&utm_campaign=" + campaign+"&utm_content="+content
             self.li = link + "?umt_source=linkedin&utm_medium=social&utm_campaign=" + campaign+"&utm_content="+content
             self.mail = link + "?umt_source=newsletter&utm_medium=email&utm_campaign=" + campaign+"&utm_content="+content
+
+
+class MakeUTMVMJ:
+    def __init__(self, link):
+        page_response = requests.get(link, timeout=10).text
+        page_content = BeautifulSoup(page_response, 'lxml').select('#jd_reference')
+        data_into_str = page_content[0].text.strip()
+        ref = data_into_str
+        self.result_link = link+"?utm_source=linkedin&utm_medium=social&utm_camaign=vmj&utm_content="+ref
+
 
 
 if __name__ == '__main__':
