cda3d622ab21b931736e990b68560065e5f2a05b
Rafal <pawelec.rafal@gmail.com>
2019-09-26 11:31:06 +0200

    changes umt to utm fix

--- app.py
+++ app.py
@@ -127,28 +127,28 @@ class MakeUTMVMJ_HU:
 class MakeUTMlink:
     def __init__(self, link, campaign, content):
         if content == "":
-            self.fb = link + "?umt_source=facebook&utm_medium=social&utm_campaign=" + campaign
+            self.fb = link + "?utm_source=facebook&utm_medium=social&utm_campaign=" + campaign
             print(self.fb)
             # data = requests.get(CALL_BILTY_API.format(API_TOKEN, self.fb)).json()
             # self.fb_short = (data['data']['url'])
 
-            self.cpc = link + "?umt_source=facebook&utm_medium=cpc&utm_campaign=" + campaign
+            self.cpc = link + "?utm_source=facebook&utm_medium=cpc&utm_campaign=" + campaign
             # data = requests.get(CALL_BILTY_API.format(API_TOKEN, self.cpc)).json()
             # self.cpc_short = (data['data']['url'])
 
-            self.li = link + "?umt_source=linkedin&utm_medium=social&utm_campaign=" + campaign
+            self.li = link + "?utm_source=linkedin&utm_medium=social&utm_campaign=" + campaign
             # data = requests.get(CALL_BILTY_API.format(API_TOKEN, self.li)).json()
             # self.li_short = (data['data']['url'])
 
-            self.mail = link+"?umt_source=newsletter&utm_medium=email&utm_campaign="+campaign
+            self.mail = link+"?utm_source=newsletter&utm_medium=email&utm_campaign="+campaign
             # data = requests.get(CALL_BILTY_API.format(API_TOKEN, self.mail)).json()
             # self.mail_short = (data['data']['url'])
 
         else:
-            self.fb = link + "?umt_source=facebook&utm_medium=social&utm_campaign=" + campaign+"&utm_content="+content
-            self.cpc = link + "?umt_source=facebook&utm_medium=cpc&utm_campaign=" + campaign+"&utm_content="+content
-            self.li = link + "?umt_source=linkedin&utm_medium=social&utm_campaign=" + campaign+"&utm_content="+content
-            self.mail = link + "?umt_source=newsletter&utm_medium=email&utm_campaign=" + campaign+"&utm_content="+content
+            self.fb = link + "?utm_source=facebook&utm_medium=social&utm_campaign=" + campaign+"&utm_content="+content
+            self.cpc = link + "?utm_source=facebook&utm_medium=cpc&utm_campaign=" + campaign+"&utm_content="+content
+            self.li = link + "?utm_source=linkedin&utm_medium=social&utm_campaign=" + campaign+"&utm_content="+content
+            self.mail = link + "?utm_source=newsletter&utm_medium=email&utm_campaign=" + campaign+"&utm_content="+content
 
 
 
